48bab477b7922ca7306fcf83a4cc08ba36c005d6
alex <alexandrutrifan17@gmail.com>
2024-07-29 17:35:24 +0300

    ctrl-f search

--- textEd.c
+++ textEd.c
@@ -208,6 +208,20 @@ int editorRowCxToRx(erow *row, int cx) {
   return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (EDITOR_TAB_STOP -1) - (cur_rx % EDITOR_TAB_STOP);
+    cur_rx++;
+
+    if (cur_rx > rx)
+      return cx;
+  }
+  return cx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
@@ -408,6 +422,28 @@ void editorSave() {
 
   free(buf);
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFind() {
+  char *query = editorPrompt("Search: %s (I'll do my best to find it ^_^)");
+  if (query == NULL)
+    return;
+  
+  int i;
+  for (i = 0; i < E.numrows; i++) {
+    erow *row = &E.row[i];
+    char *match = strstr(row->render, query);
+    if (match) {
+      E.cy = i;
+      E.cx = editorRowRxToCx(row, match - row->render);
+      E.rowoff = E.numrows;
+      break;
+    }
+  }
+
+  free(query);
 }
 
 /*** append buffer ***/
@@ -666,6 +702,10 @@ void editorProcessKeypress() {
           E.cx = E.row[E.cy].size;
         break;
 
+      case CTRL_KEY('f'):
+        editorFind();
+        break;
+
       case BACKSPACE:
       case CTRL_KEY('h'):
       case DEL_KEY:
@@ -732,7 +772,7 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: CTRL-S = save | CTRL-Q = quit");
+  editorSetStatusMessage("HELP: CTRL-S = save | CTRL-Q = quit | CTRL-F = find");
 
   while (1) {
     editorRefreshScreen();
